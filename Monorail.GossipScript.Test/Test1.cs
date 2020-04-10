8af4d8c9bf41107258d5aa71647e2a44fef42e95
jpsmyth <joshuapaulsmyth@gmail.com>
2020-04-10 13:42:56 +1200

    Added Test Framework Project for Monorail.GossipScript

---
+++ Monorail.GossipScript.Test/Test1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Monorail.GossipScript.Test
+{
+    [TestFixture]
+    public class Test1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = false;
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+}
